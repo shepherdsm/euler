85a83e336a6243723bd364a961c32889af5b8bef
Scott Shepherd <scottmshepherd@gmail.com>
2014-10-08 23:16:01 -0400

    Added Under function to fib.

--- fib/fib.go
+++ fib/fib.go
@@ -2,21 +2,37 @@
 package fib
 
 // Nth returns the Nth fibonacci number.
-func Nth(n uint) (uint) {
-    if n == 0 {
-        return 0
-    }
+func Nth(n uint) uint {
+	if n == 0 {
+		return 0
+	}
 
-    var nPrev uint = 0
-    var nCur uint = 1
-    var i uint = 0
+	var nPrev uint = 0
+	var nCur uint = 1
+	var i uint = 0
 
-    for i = 1; i < n; i++ {
-        tmp := nCur
+	for i = 1; i < n; i++ {
+		tmp := nCur
 
-        nCur += nPrev
-        nPrev = tmp
-    }
+		nCur += nPrev
+		nPrev = tmp
+	}
 
-    return nCur
+	return nCur
+}
+
+// Generates all the fibonacci numbers under, but not including upper.
+func Under(upper uint) []uint {
+	var results []uint
+	var n uint = 0
+
+	fib := Nth(n)
+
+	for fib < upper {
+		results = append(results, fib)
+		n++
+		fib = Nth(n)
+	}
+
+	return results
 }
--- fib/fib_test.go
+++ fib/fib_test.go
@@ -1,30 +1,58 @@
 package fib
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-type testPair struct {
-    n uint
-    want uint
-}
-
-var tests = []testPair {
-    {0, 0},
-    {1, 1},
-    {2, 1},
-    {10, 55},
-    {50, 12586269025},
+type testNth struct {
+	n    uint
+	want uint
 }
 
 func TestNth(t *testing.T) {
-    for _, pair := range tests {
-        got := Nth(pair.n)
+	var tests = []testNth{
+		{0, 0},
+		{1, 1},
+		{2, 1},
+		{10, 55},
+		{50, 12586269025},
+	}
 
-        if got != pair.want {
-            t.Error(
-                "For", pair.n,
-                "got", got,
-                "expected", pair.want,
-            )
-        }
-    }
+	for _, pair := range tests {
+		got := Nth(pair.n)
+
+		if got != pair.want {
+			t.Error(
+				"For", pair.n,
+				"got", got,
+				"expected", pair.want,
+			)
+		}
+	}
+}
+
+type testUnder struct {
+	upper uint
+	want  []uint
+}
+
+func TestUpper(t *testing.T) {
+	var tests = []testUnder{
+		{0, nil},
+		{1, []uint{0}},
+		{10, []uint{0, 1, 1, 2, 3, 5, 8}},
+	}
+
+	for _, pair := range tests {
+		got := Under(pair.upper)
+
+		if !reflect.DeepEqual(got, pair.want) {
+			t.Error(
+				"For", pair.upper,
+				"got", got,
+				"expected", pair.want,
+			)
+		}
+	}
 }
