2015bfef77b27edbdee9901ad2b1a47d0faa1e5f
Scott Shepherd <scottmshepherd@gmail.com>
2014-10-09 22:35:33 -0400

    Added collatz sequence function.

---
+++ collatz.go
@@ -0,0 +1,20 @@
+package euler
+
+func Collatz(seed uint) []uint {
+    var results = []uint{seed}
+
+    for seed != 1 {
+        seed = nextNum(seed)
+        results = append(results, seed)
+    }
+
+    return results
+}
+
+func nextNum(n uint) uint {
+    if n % 2 == 0 {
+        return n / 2
+    } else {
+        return 3 * n + 1
+    }
+}
