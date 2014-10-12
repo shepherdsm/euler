8e396b75cb52e442e566ecc95598d89068160935
Scott Shepherd <scottmshepherd@gmail.com>
2014-10-12 15:48:07 -0400

    Added test for nextNum.

--- all_test.go
+++ all_test.go
@@ -31,10 +31,31 @@ var collatz = [][]uint{
 	{44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1},
 }
 
+var nextnum = []uint{
+	0,
+	4,
+	1,
+	2,
+	16,
+	22,
+	5,
+	8,
+	10,
+	22,
+}
+
 func TestCollatz(t *testing.T) {
 	for i, num := range funcVals {
 		if got := Collatz(num); !reflect.DeepEqual(got, collatz[i]) {
 			t.Errorf("Collatz(%d) = %v, want %v", num, got, collatz[i])
+		}
+	}
+}
+
+func TestNextNum(t *testing.T) {
+	for i, num := range funcVals {
+		if got := nextNum(num); got != nextnum[i] {
+			t.Errorf("nextNum(%d) = %d, want %d", num, got, nextnum[i])
 		}
 	}
 }
